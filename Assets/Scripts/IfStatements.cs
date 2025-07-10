2602c6868aaea662e3c3a8782251f9f3c507576c
zinine73 <zinine@naver.com>
2025-07-10 12:39:51 +0900

    IF

---
+++ Assets/Scripts/IfStatements.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+    float coffeeTemperature = 85.0f;
+    float hotLimitTemperature = 70.0f;
+    float coldLimitTemperature = 40.0f;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            TemperatureTest();
+
+        coffeeTemperature -= Time.deltaTime * 1f;
+    }
+
+    void TemperatureTest()
+    {
+        if (coffeeTemperature > hotLimitTemperature)
+        {
+            print("Coffee is too hot.");
+        }
+        else if (coffeeTemperature < coldLimitTemperature)
+        {
+            print("Coffee is too cold.");
+        }
+        else
+        {
+            print("Coffee is just right.");
+        }
+    }
+}
