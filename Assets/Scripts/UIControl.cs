42265607928a9cb6d2a4285fe26c35824143f5d0
zinine73 <zinine@naver.com>
2025-09-22 11:59:03 +0900

    UI scene

---
+++ Assets/Scripts/UIControl.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIControl : MonoBehaviour
+{
+    [SerializeField] TMP_Text titleText;
+    [SerializeField] Toggle toggle1;
+    [SerializeField] Slider slider;
+    [SerializeField] Button btn;
+    [SerializeField] TMP_Dropdown dropdown;
+    void Start()
+    {
+        titleText.text = "This is Title";
+    }
+
+    public void OnToggle1Clicked()
+    {
+        Debug.Log("Clicked toggle1");
+        if (toggle1.isOn)
+        {
+            Debug.Log("Toggle1 is on.");
+        }
+    }
+
+    public void OnSliderClicked()
+    {
+        Debug.Log(slider.value);
+    }
+
+    public void OnBtnClicked()
+    {
+        Debug.Log("Button clicked!");
+    }
+
+    public void OnDropdownClicked()
+    {
+        Debug.Log(dropdown.value);
+    }
+}
